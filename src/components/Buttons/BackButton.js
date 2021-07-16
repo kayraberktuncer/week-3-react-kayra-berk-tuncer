81ae66901bbea7c7205992b57dd25a160b06d7ec
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2021-07-16 15:43:47 +0300

    Update DetailPage Style

--- src/components/Buttons/BackButton.js
+++ src/components/Buttons/BackButton.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function BackButton() {
   return (
     <Link className="back-button" to="/">
-      <span>Back</span>
+      {'<'}
     </Link>
   )
 }
