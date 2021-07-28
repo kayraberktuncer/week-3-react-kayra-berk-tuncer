be61fc06aea2e667fe21cf9af08cd6d894840578
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2021-07-28 13:55:47 +0300

    Add Get Single Character Episodes test

--- src/api/service.js
+++ src/api/service.js
@@ -1,6 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios'
 
+export function getData() {
+  return axios
+    .get('https://rickandmortyapi.com/api/character/1')
+    .then((result) => result.data.id)
+}
+
 export function getCharacters(characters, setCharacters, query) {
   axios
     .get(`https://rickandmortyapi.com/api/character${query}`)
@@ -15,8 +21,8 @@ export function getSingleCharacter(id, setCharacter) {
     .then(({ data }) => setCharacter(data))
 }
 
-export function getSingleCharacterEpisodes(character, setEpisodes) {
-  axios
+export function getSingleCharacterEpisodes(character) {
+  return axios
     .all([...character.episode.map((episode) => axios.get(episode))])
-    .then((result) => setEpisodes(result.map((episode) => episode.data)))
+    .then((result) => result)
 }
